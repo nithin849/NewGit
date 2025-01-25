268bd1c8bdf880c269d449624f803cb2e765e8df
nithin849 <mnithinmangali@gmail.com>
2025-01-25 22:00:01 +0530

    Added method in code

--- NewRepo.java
+++ NewRepo.java
@@ -2,5 +2,9 @@ package GitLearning;
 public class NewRepo{
     public static void main(String[] args) {
         System.out.println("New Repo done using Git Commands");
+        System.out.println(find(5));
+    }
+    public static int find(int n){
+        return n*n;
     }
 }
